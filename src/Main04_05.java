6a1b2af554bdbeb6f3dfaf45cef89b40333a6b24
john hai <haijchn@gmail.com>
2020-07-01 15:03:13 +0800

    2020.07.01

--- src/Main04_05.java
+++ src/Main04_05.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main04_05 {
     /**
      * 实现一个函数，检查一棵二叉树是否为二叉搜索树。
@@ -18,28 +21,63 @@ public class Main04_05 {
         TreeNode(int x) { val = x; }
     }
 
-    public static boolean isValidBST(TreeNode root) {
+    //方法一：采用中序遍历，将待判断的树的节点放在ArrayList中，然后再判断是否是递增的序列（leetcode上要求后边的元素不能大于等于前一个元素，只能大于）。
+    // 因为是采用了额外的空间来保存所有节点，并且进行比较，所以复杂度比较高。不如方法二。
+    static ArrayList<Integer> list;
+    public static boolean isValidBST1(TreeNode root){
         if(root == null){
             return true;
-        }else{
-            //左右节点都为空
-            if(root.left == null && root.right == null)
-                return isValidBST(root.left) && isValidBST(root.right);
-            //左节点为空，右节点不为空
-            if(root.left == null && root.right != null)
-                return isValidBST(root.left) && root.right.val >= root.val && isValidBST(root.right);
-            //右节点为空，左节点不为空
-            if(root.right == null && root.left != null)
-                return isValidBST(root.right) && root.left.val < root.val && isValidBST(root.left);
-            //左节点，右节点都不为空
-            if(root.left != null && root.right != null)
-                return isValidBST(root.left) && root.right.val >= root.val && root.left.val < root.val && isValidBST(root.right);
+        }
+        list = new ArrayList<>();
+        middleOrderTraversel(root); //调用中序遍历
+        int min = Integer.MIN_VALUE;
+        for(int i = 0; i < list.size(); i++){
+            if (list.get(i) <= min) {
+                if(i == 0 && list.get(i) == Integer.MIN_VALUE){ //这里注意，对根节点元素为Integer.MIN_VALUE的情况做一下特判，否则{Integer.MIN_VALUE,null,null}无法通过
+                    min = list.get(i);
+                }
+                else{
+                    return false;
+                }
+            } else {
+                min = list.get(i);
+            }
+        }
+        return true;
+    }
+
+    /**
+     *中序遍历之递归实现
+     */
+    public static void middleOrderTraversel(TreeNode root){
+        if(root != null){
+            middleOrderTraversel(root.left);
+            list.add(root.val);
+            middleOrderTraversel(root.right);
         }
     }
+    //方法二：采用递归的方法，找寻左子树中的最右（数值最大）节点，找寻右子树中的最左（数值最小）节点，然后判断当前层是否合法。双百。
+    public static boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        TreeNode maxLeft = root.left, minRight = root.right;
+        // 找寻左子树中的最右（数值最大）节点
+        while (maxLeft != null && maxLeft.right != null)
+            maxLeft = maxLeft.right;
+        // 找寻右子树中的最左（数值最小）节点
+        while (minRight != null && minRight.left != null)
+            minRight = minRight.left;
+        // 当前层是否合法
+        boolean ret = (maxLeft == null || maxLeft.val < root.val) && (minRight == null || root.val < minRight.val);
+        // 进入左子树和右子树并判断是否合法
+        return  ret && isValidBST(root.left) && isValidBST(root.right);
+    }
+
     public static void main(String[] args){
-        TreeNode root = new TreeNode(2);
-        root.left = new TreeNode(1);
-        root.right = new TreeNode(3);
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(20);
         System.out.println(isValidBST(root));
     }
 }
